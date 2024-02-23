2fd13cd75bf072668b12f8ce4cbe7cbc5930c8d6
rizwankhan <super047240@gmail.com>
2024-02-23 21:36:42 +0530

    intersection

---
+++ 02-basic-challenges-2/02-array-intersection/array-intersection-run.js
@@ -0,0 +1,4 @@
+const arrayIntersection = require('./array-intersection')
+
+const result = arrayIntersection([1, 2, 3, , 4, 4, 5], [1, 2, 7, 8, 9, 10])
+console.log(result)
---
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -0,0 +1,21 @@
+// function arrayIntersection(arr1, arr2) {
+//   const allArray = []
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !allArray.includes(arr1[i])) {
+//       allArray.push(arr1[i])
+//     }
+//   }
+//   return allArray
+// }
+function arrayIntersection(arr1, arr2) {
+  const set1 = new Set(arr1)
+  const intersectionArr = []
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num)
+    }
+  }
+  return intersectionArr
+}
+module.exports = arrayIntersection

ae8cddabd73f260c3b9db72ba9020da0a6f6095b
rizwankhan <super047240@gmail.com>
2024-03-02 20:05:34 +0530

    sum of square

---
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-even-square-run.js
@@ -0,0 +1,8 @@
+// const sumEvenSqure = require('./sum-even-square')
+
+// const result = sumEvenSqure(10)
+// console.log(result)
+
+const sumOfEvenSquares = require('./sum-even-square')
+const result = sumOfEvenSquares([1, 2, 3, 4, 5, 6, 7, 8])
+console.log(result)
---
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-even-square.js
@@ -0,0 +1,9 @@
+//
+function sumOfEvenSquares(numbers) {
+  const evenSquares = numbers
+    .filter((num) => num % 2 === 0)
+    .map((sum) => sum ** 2)
+    .reduce((initial, add) => add + initial, 0)
+  return evenSquares
+}
+module.exports = sumOfEvenSquares

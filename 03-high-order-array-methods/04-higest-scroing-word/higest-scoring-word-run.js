df969a780a2bf70d0d0351f4d3b3c3a5826f3a9a
rizwankhan <super047240@gmail.com>
2024-03-05 19:25:06 +0530

    higest-scoring-word

---
+++ 03-high-order-array-methods/04-higest-scroing-word/higest-scoring-word-run.js
@@ -0,0 +1,4 @@
+const highestScoringWord = require('./higest-scroing-word')
+
+const result = highestScoringWord('Hello my name is xavier')
+console.log(result)
---
+++ 03-high-order-array-methods/04-higest-scroing-word/higest-scoring-word.test.js
@@ -0,0 +1,9 @@
+const highestScoringWord = require('./higest-scroing-word')
+
+test('Finding the highest scoring word', () => {
+  expect(highestScoringWord('hello my name is xavier')).toBe('xavier')
+  expect(highestScoringWord('what time are we climbing up the volcano')).toBe(
+    'volcano'
+  )
+  expect(highestScoringWord('take me to semynak')).toBe('semynak')
+})
